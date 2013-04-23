152f0b2fea58e93733fd78f99728694ece8c1b8a
lliss <llisscommunications@gmail.com>
2013-04-23 00:19:42 -0400

    adding in logic for the pusle boolean

--- motor_driver.ino
+++ motor_driver.ino
@@ -44,117 +44,139 @@ void setup() {
   pinMode(pulsepin, INPUT);
   pinMode(lightpin, INPUT);
   pinMode(out1, OUTPUT);
-  pinMode(out2, OUTPUT);  
+  pinMode(out2, OUTPUT);
   pinMode(out3, OUTPUT);
-  pinMode(pin, OUTPUT); 
-  pinMode(testpin, OUTPUT); 
+  pinMode(pin, OUTPUT);
+  pinMode(testpin, OUTPUT);
 
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600); 
+  Serial.begin(9600);
 }
- 
+
 void loop() {
   boolean readPulse = digitalRead(pulsepin) == HIGH ? true : false;
-  //Serial.println(readPulse);
   pulse = false;
   if (oldPinSetting != readPulse) {
     oldPinSetting = readPulse;
     pulse = true;
   }
-  if (pulse) {
-    for (int i = 0; i < 3; i++) {
-      /*digitalWrite(pin, HIGH);
-      delay(150);
-      digitalWrite(pin, LOW);
-      delay(150);*/
-    }
-  }
-  
-  if (digitalRead(dirpin1) == HIGH) {
-    /*digitalWrite(out1, HIGH);
-    digitalWrite(out2, LOW);
-    digitalWrite(out3, HIGH);
-    
-    digitalWrite(out4, HIGH);
-    digitalWrite(out5, LOW);
-    digitalWrite(out6, HIGH);*/
-    Serial.println("high");
-  }
-  else {
-    /*digitalWrite(out1, LOW);
-    digitalWrite(out2, LOW);
-    digitalWrite(out3, HIGH);
-    
-    digitalWrite(out4, LOW);
-    digitalWrite(out5, LOW);
-    digitalWrite(out6, HIGH);*/
-    Serial.println("low");
-  }
-  //setForward();
-  //setReverse();
-  //setLeft();
-  //setRight();
-  delay(2000);
-  setBreak();
-  delay(2000);
-  //setReverse();
-  delay(2000);
-  //setLeft();
-  delay(2000);
-  //setRight();
-  delay(2000);
+
+  int p1 = (int) digitalRead(dirpin1);
+  int p2 = (int) digitalRead(dirpin2);
+  boolean cease = (boolean) digitalRead(breakpin);
+  parseAndActivate(p1, p2, false, false);
 }
 
-void setForward() {
+/**
+ * @todo
+ */
+void setForward(boolean pulse) {
   digitalWrite(out1, LOW);
   digitalWrite(out2, HIGH);
   digitalWrite(out3, HIGH);
-    
+
   digitalWrite(out4, LOW);
   digitalWrite(out5, HIGH);
   digitalWrite(out6, HIGH);
+
+  if (pulse) {
+    delay(1000);
+    setBreak();
+  }
 }
 
-void setReverse() {
+/**
+ * @todo
+ */
+void setReverse(boolean pulse) {
   digitalWrite(out1, HIGH);
   digitalWrite(out2, LOW);
   digitalWrite(out3, HIGH);
-    
+
   digitalWrite(out4, HIGH);
   digitalWrite(out5, LOW);
   digitalWrite(out6, HIGH);
+
+  if (pulse) {
+    delay(1000);
+    setBreak();
+  }
 }
 
+/**
+ * @todo
+ */
 void setBreak() {
   digitalWrite(out1, LOW);
   digitalWrite(out2, LOW);
   digitalWrite(out3, HIGH);
-    
+
   digitalWrite(out4, LOW);
   digitalWrite(out5, LOW);
   digitalWrite(out6, HIGH);
 }
 
-void setRight() {
+/**
+ * @todo
+ */
+void setRight(boolean pulse) {
   digitalWrite(out1, HIGH);
   digitalWrite(out2, LOW);
   digitalWrite(out3, HIGH);
-    
+
   digitalWrite(out4, LOW);
   digitalWrite(out5, HIGH);
   digitalWrite(out6, HIGH);
+
+  if (pulse) {
+    delay(1000);
+    setBreak();
+  }
 }
 
-void setLeft() {
+/**
+ * @todo
+ */
+void setLeft(boolean pulse) {
   digitalWrite(out1, LOW);
   digitalWrite(out2, HIGH);
   digitalWrite(out3, HIGH);
-    
+
   digitalWrite(out4, HIGH);
   digitalWrite(out5, LOW);
   digitalWrite(out6, HIGH);
+
+  if (pulse) {
+    delay(1000);
+    setBreak();
+  }
 }
 
-void parseAction(int p1, int p2, boolean pulse) {
-   
+/**
+ * @todo
+ *
+ * 00 Forward
+ * 01 Right
+ * 10 Left
+ * 11 Reverse
+ */
+void parseAndActivate(int p1, int p2, boolean cease, boolean pulse) {
+  if (cease) {
+    setBreak();
+    return;
+  }
+  pulse = false;
+  int sum = p1 + p2;
+  if (sum == 0) {
+    setForward(pulse);
+  }
+  else if(sum == 2) {
+    setReverse(pulse);
+  }
+  else if (p1 == 1) {
+    setLeft(pulse);
+  }
+  else {
+    setRight(pulse);
+  }
 }
